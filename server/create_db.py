e1f381ec3a2841facf6a2184541fb056e42ba4ad
Alex Thompson <godfoder@acis.ufl.edu>
2016-03-16 17:15:20 -0400

    rip out sqlalchemy migrate, replace with alembic probably

--- server/create_db.py
+++ server/create_db.py
@@ -1,23 +1,11 @@
-from migrate.versioning import api
-from migrate.exceptions import DatabaseAlreadyControlledError
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
 
 
 def create_or_update_db():
-    try:
-        import os.path
-        db.create_all()
-        if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-            api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-            api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)  # noqa
-        else:
-            api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,  # noqa
-                                api.version(SQLALCHEMY_MIGRATE_REPO)
-                                )
-    except DatabaseAlreadyControlledError:
-        pass
+    import os.path
+    db.create_all()
 
 if __name__ == '__main__':
     create_or_update_db()
--- server/models/appuser.py
+++ server/models/appuser.py
@@ -7,6 +7,7 @@ class AppUser(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_uuid = db.Column(db.Text, nullable=False, unique=True)
+    user_alias = db.Column(db.Text, unique=True)
     auth_key = db.Column(db.Text, nullable=False)
     config = db.Column(db.Text, default="{}")
     login_date = db.Column(db.DateTime)
