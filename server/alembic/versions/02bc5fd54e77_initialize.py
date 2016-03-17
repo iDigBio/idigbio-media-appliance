bea74197bda92bf759b8fda8107092ab21319b34
Alex Thompson <godfoder@acis.ufl.edu>
2016-03-17 09:59:23 -0400

    init alembic.

---
+++ server/alembic/versions/02bc5fd54e77_initialize.py
@@ -0,0 +1,24 @@
+"""initialize.
+
+Revision ID: 02bc5fd54e77
+Revises: 
+Create Date: 2016-03-17 09:39:59.216280
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '02bc5fd54e77'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
