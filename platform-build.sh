f27aeff2d4f5cf9a0a388793c9866280168858b7
Alex Thompson <godfoder@acis.ufl.edu>
2016-04-04 16:24:18 -0400

    conda platform build script.

---
+++ platform-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r ~/conda-builds
+mkdir ~/conda-builds
+mkdir ~/conda-builds/platform-pack/
+
+cd ~/conda-builds
+
+packages='pytest-cov python-editor aniso8601 blinker flask-restful flask-sqlalchemy appdirs alembic easygui'
+
+for p in $packages; do
+    rm ~/miniconda3/conda-bld/linux-64/$p*
+    conda skeleton pypi $p;
+    conda build --no-test $p;
+    conda convert --platform all `ls ~/miniconda3/conda-bld/linux-64/$p*` -o ~/conda-builds/platform-pack/;
+done
+
+for p in `ls ~/conda-builds/platform-pack/*/*`; do
+    anaconda upload --user idigbio $p;
+done
