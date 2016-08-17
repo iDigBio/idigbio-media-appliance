204c08c986d1d991c8186031f09ea520fe3dd893
Alex Thompson <godfoder@acis.ufl.edu>
2016-08-17 15:10:38 -0400

    fix tests for windows.

--- idigbio_media_appliance/tests/test_guids.py
+++ idigbio_media_appliance/tests/test_guids.py
@@ -5,6 +5,7 @@ from ..lib.dir_handling import guid_mode
 import uuid
 import re
 import os
+import pathlib
 
 
 def test_uuid(datadir):
@@ -20,17 +21,17 @@ def test_hash(datadir):
 
 def test_prefix_fullpath(datadir):
     prefix = "test:"
-    path = datadir + "/images/image1.jpg"
-    params = (prefix, path)
+    path = pathlib.Path(datadir, "/images/image1.jpg")
+    params = (prefix, str(path))
 
     g = guid_mode["fullpath"](*params)
-    assert g == prefix + path
+    assert g == prefix + str(path)
 
 
 def test_prefix_filename(datadir):
     prefix = "test:"
-    path = datadir + "/images/image1.jpg"
-    params = (prefix, path)
+    path = pathlib.Path(datadir, "/images/image1.jpg")
+    params = (prefix, str(path))
 
     g = guid_mode["filename"](*params)
-    assert g == prefix + os.path.basename(path)
+    assert g == prefix + str(path.name)
