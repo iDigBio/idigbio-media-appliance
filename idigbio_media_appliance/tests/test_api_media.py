12620e4719ffc6e4e216685551c18e85cb5f2824
Alex Thompson <godfoder@acis.ufl.edu>
2016-08-17 15:16:44 -0400

    more text updates.

--- idigbio_media_appliance/tests/test_api_media.py
+++ idigbio_media_appliance/tests/test_api_media.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, print_function, division, unicode_litera
 import os
 import json
 from flask import url_for
+import pathlib
 
 
 def test_media_api_list(api_login, json_in_out, client):
@@ -12,36 +13,36 @@ def test_media_api_list(api_login, json_in_out, client):
 
 
 def test_media_api_post(api_login, json_in_out, client, datadir):
-    image_path = os.path.join(datadir, "images/image1.jpg")
-    image_ref = "TESTDATA:" + image_path.split("/")[-1]
+    image_path = pathlib.Path(datadir, "images", "image1.jpg")
+    image_ref = "TESTDATA:" + str(image_path.name)
     res = client.post(url_for('media_api.mediaapi'), data=json.dumps({
-        "path": image_path,
-        "file_reference": "TESTDATA:" + image_path.split("/")[-1]
+        "path": str(image_path),
+        "file_reference": image_ref
     }), headers=json_in_out)
     assert res.json.get("id") is not None
-    assert res.json.get("path") == image_path
+    assert res.json.get("path") == str(image_path)
     assert res.json.get("file_reference") == image_ref
 
 
 def test_media_api_post_nologin(json_in_out, client, datadir):
-    image_path = os.path.join(datadir, "images/image1.jpg")
-    image_ref = "TESTDATA:" + image_path.split("/")[-1]
+    image_path = pathlib.Path(datadir, "images", "image1.jpg")
+    image_ref = "TESTDATA:" + str(image_path.name)
     res = client.post(url_for('media_api.mediaapi'), data=json.dumps({
-        "path": image_path,
-        "file_reference": "TESTDATA:" + image_path.split("/")[-1]
+        "path": str(image_path),
+        "file_reference": image_ref
     }), headers=json_in_out)
     assert res.status_code == 401
 
 
 def test_media_api_post_get(api_login, json_in_out, client, datadir):
-    image_path = os.path.join(datadir, "images/image1.jpg")
-    image_ref = "TESTDATA:" + image_path.split("/")[-1]
+    image_path = pathlib.Path(datadir, "images", "image1.jpg")
+    image_ref = "TESTDATA:" + str(image_path.name)
     res = client.post(url_for('media_api.mediaapi'), data=json.dumps({
-        "path": image_path,
-        "file_reference": "TESTDATA:" + image_path.split("/")[-1]
+        "path": str(image_path),
+        "file_reference": image_ref
     }), headers=json_in_out)
     assert res.json.get("id") is not None
-    assert res.json.get("path") == image_path
+    assert res.json.get("path") == str(image_path)
     assert res.json.get("file_reference") == image_ref
 
     get_res = client.get(url_for('media_api.mediaapi', id=res.json.get("id")))
