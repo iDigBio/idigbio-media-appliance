4c445c13be3d3413bca7a9b7f5d83fc48e3c36d4
Dan Stoner <danstoner@gmail.com>
2017-01-31 09:44:31 -0500

    add comment about real file path for sqlite db

--- idigbio_media_appliance/config.py
+++ idigbio_media_appliance/config.py
@@ -10,6 +10,8 @@ USER_DATA = appdirs.user_data_dir("media_appliance", "idigbio")
 if not os.path.exists(USER_DATA):
     os.makedirs(USER_DATA)
 
+# On Windows 7, the db will typically end up in C:\Users\<user>\AppData\Local\idigbio\media_appliance
+# The "AppData" folder is not visible by default, use "Folder Options" to "Show all folders" in Navigation Pane
 DATABASE_FILE = os.path.join(USER_DATA, "local.db")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
